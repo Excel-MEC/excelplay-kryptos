f6e31432fc0d38b8d8f2ae6d7667d70153567b9a
kurianbenoy <kurian.pro@gmail.com>
2018-10-07 07:56:32 +0530

    Made Answer accept files also

--- excelplay_kryptos/api/serializers.py
+++ excelplay_kryptos/api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from .models import Level, KryptosUser
-
+from django.forms.fields import FileField
 
 
 class LevelSerializer(serializers.ModelSerializer):
+    file = FileField()
     class Meta:
         fields = (
             'id',
