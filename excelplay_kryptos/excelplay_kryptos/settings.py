0f9f21606d2a0300273234b13a53a4b4d7275a8a
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-13 11:14:08 +0530

    Update session config

--- excelplay_kryptos/excelplay_kryptos/settings.py
+++ excelplay_kryptos/excelplay_kryptos/settings.py
@@ -135,8 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 SESSION_ENGINE = 'redis_sessions.session'
 SESSION_REDIS = {
-    'host': None,
-    'unix_domain_socket_path': '/var/run/redis/redis.sock',
+    'host': 'localhost',
+    'port: 6379',
     'db': 0,
     'prefix': 'session',
     'socket_timeout': 10
