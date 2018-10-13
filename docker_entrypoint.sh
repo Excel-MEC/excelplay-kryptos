2e93f69896cb8d284e3401618c6f40c3a9d2ca59
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-13 13:26:53 +0530

    Update entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,4 +2,6 @@
 
 python excelplay-kryptos/excelplay_kryptos/manage.py makemigrations && \
 	python excelplay-kryptos/excelplay_kryptos/manage.py migrate
-gunicorn excelplay-kryptos/excelplay_kryptos/excelplay_kryptos.wsgi --bind 0.0.0.0:8001
+cd excelplay-kryptos/excelplay_kryptos
+gunicorn excelplay_kryptos.wsgi --bind 0.0.0.0:8001
+
