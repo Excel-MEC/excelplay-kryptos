d6c9e1f67e1f0c9ed5f1432cda7d3556255b8b44
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-13 08:29:30 +0530

    Update models, remove User calls

--- excelplay_kryptos/api/models.py
+++ excelplay_kryptos/api/models.py
@@ -1,45 +1,30 @@
 from django.db import models
 
-# Create your models here.
-
-class User(models.Model):
-	#TODO: User model to be imported from core auth
-	user_id = models.CharField(primary_key=True,max_length=100)
-	username = models.CharField(max_length=100)
-	profile_picture = models.URLField()
-	email = models.EmailField()
-	points = models.IntegerField(default=0)
-
-	def __repr__(self):
-		return self.username
-	class Meta:
-		ordering = ['user_id','points']
-
-
 
 class Level(models.Model):
-	options = (
-	   ('I', 'Image'),
-	   ('NI','Not Image')
-	)
-	level = models.IntegerField(default =1)
-	answer = models.TextField()
-	source_hint = models.TextField(blank=True,null=True)
-	level_file =  models.FileField(upload_to = 'level_images/',null=True)
-	filetype = models.CharField(max_length = 10,choices=options,default='Image',blank=True)
-	def __str__(self):
-		return str(self.level)
+    options = (
+	('I', 'Image'),
+	('NI','Not Image')
+    )
+    level = models.IntegerField(default =1)
+    answer = models.TextField()
+    source_hint = models.TextField(blank=True,null=True)
+    level_file =  models.FileField(upload_to = 'level_images/',null=True)
+    filetype = models.CharField(max_length = 10,choices=options,default='Image',blank=True)
+	
+    def __str__(self):
+	return str(self.level)
 
 class KryptosUser(models.Model):
-	user_id = models.OneToOneField(User,primary_key=True, on_delete=models.CASCADE)
-	level = models.IntegerField(default=1)
-	rank = models.IntegerField(default=10000)
-	last_anstime = models.DateTimeField()
+    user_id = models.CharField(primary_key=True, max_length=100)
+    level = models.IntegerField(default=1)
+    rank = models.IntegerField(default=10000)
+    last_anstime = models.DateTimeField()
 
-	def __str__(self):
-		return str(self.rank)
+    def __str__(self):
+	return '<{0}: {1}>'.format(self.user_id, self.rank)
 
 
 class SubmittedAnswer(models.Model):
-	kryptosUser = models.ForeignKey(KryptosUser, on_delete=models.CASCADE)
-	answers = models.TextField()
+    kryptosUser = models.ForeignKey(KryptosUser, on_delete=models.CASCADE)
+    answers = models.TextField()
