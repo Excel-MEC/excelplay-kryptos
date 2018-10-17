0652964d0be9086b5d79175e10b65202bc81f874
Kurian Benoy <kurian.bkk@gmail.com>
2018-10-17 22:37:20 +0530

    Update STATIC_FILE_DIRS

--- excelplay_kryptos/excelplay_kryptos/settings.py
+++ excelplay_kryptos/excelplay_kryptos/settings.py
@@ -16,8 +16,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR,"static"), 
+        "/var/www/static/",
     ]
+
 
 # False if not in os.environ
 #DEBUG = env('DEBUG')
