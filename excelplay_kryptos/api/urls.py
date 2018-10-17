539c34aef4c700c59d48b1c89f2d7e436ef44333
Augustine Tharakan <augustinetharakan12@gmail.com>
2018-10-17 12:01:38 +0530

    removed '/'

--- excelplay_kryptos/api/urls.py
+++ excelplay_kryptos/api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('ask/', views.ask),
-    path('answer/', views.answer),
-    path('ranklist/', views.leaderboard),
-    path('test/', views.test_session)
+    path('ask', views.ask),
+    path('answer', views.answer),
+    path('ranklist', views.leaderboard),
+    path('test', views.test_session)
 ]
