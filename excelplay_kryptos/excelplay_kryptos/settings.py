220fb27edcb0f0dc432ab6083120a1ebbe20b325
Siva R <sivagrenganathan+github@protonmail.com>
2018-10-18 10:50:54 +0530

    Remove staticfile dirs and media root

--- excelplay_kryptos/excelplay_kryptos/settings.py
+++ excelplay_kryptos/excelplay_kryptos/settings.py
@@ -15,11 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"), 
-        "/var/www/static/",
-    ]
-
 
 # False if not in os.environ
 #DEBUG = env('DEBUG')
@@ -132,11 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
 
 # Session settings
 
