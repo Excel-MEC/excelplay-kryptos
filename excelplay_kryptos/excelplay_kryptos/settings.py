df716d94c38064c655c107d037b5a53501707114
Augustine <augustinetharakan12@gmail.com>
2019-10-07 18:50:38 +0530

    changed django static files location

--- excelplay_kryptos/excelplay_kryptos/settings.py
+++ excelplay_kryptos/excelplay_kryptos/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
 MEDIA_URL = '/media/'
 
 # Session settings
