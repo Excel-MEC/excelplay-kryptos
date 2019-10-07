e62260c492f17de459fe6173e375a7e5563cea8b
Augustine <augustinetharakan12@gmail.com>
2019-10-07 19:30:30 +0530

    docker-entrypoint changes (added migration command for api model

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 python excelplay-kryptos/excelplay_kryptos/manage.py makemigrations && \
+	python excelplay-kryptos/excelplay_kryptos/manage.py makemigrations api && \
 	python excelplay-kryptos/excelplay_kryptos/manage.py migrate
 cd excelplay-kryptos/excelplay_kryptos
 gunicorn excelplay_kryptos.wsgi --bind 0.0.0.0:8001
