0da70b6093318f90ac78fd1eaaf55361f0d00c12
Augustine <augustinetharakan12@gmail.com>
2019-10-11 18:02:36 +0530

    added feature for removing user from redis leaderboard when deleted using django admin dashboard

--- excelplay_kryptos/api/admin.py
+++ excelplay_kryptos/api/admin.py
@@ -1,3 +1,5 @@
+from redis_leaderboard.wrapper import RedisLeaderboard
+
 from django.contrib import admin
 from .models import(
     Level,
@@ -7,6 +9,7 @@ from .models import(
 )
 # Register your models here.
 
+rdb = RedisLeaderboard('redis', 6379, 0)
 
 class HintsInline(admin.StackedInline):
     model = Hint
@@ -18,13 +21,19 @@ class LevelAdmin(admin.ModelAdmin):
     inlines = (HintsInline,)
     list_display = ('level', 'source_hint', 'level_file')
 
+
 class AnswerLogAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'level', 'answer', 'anstime')
     search_fields = ('user_id',)
 
+
 class KryptosUserAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'level', 'last_anstime')
     search_fields = ('user_id',)
+
+    def delete_model(self, request, obj):
+        rdb.remove('kryptos', obj.user_id)
+        super().delete_model(request, obj)
 
 
 admin.site.register(Level, LevelAdmin)
